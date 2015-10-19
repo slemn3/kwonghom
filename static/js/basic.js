4b77e918d979323bc21e2f1ae456cd41425bf7bc
slemn3 <foggydoggy33@gmail.com>
2015-10-18 21:18:25 -0700

    on click event

--- static/js/basic.js
+++ static/js/basic.js
@@ -1,25 +1,26 @@
 $(document).ready(function(){
 	var height = $(window).height();
 	$(".fittoheight").css("height", height);
+});
 
-	$('li a[href*="more"]').bind('click', function(event){
-		console.log('trigger');
+$("button").bind('click', function(event){submitComment()})
 
-	   $.ajax({
-		    url: "http://ec2-54-149-119-45.us-west-2.compute.amazonaws.com:3000?callback=paintComments",
-		    type: 'GET',
-		    dataType: 'jsonp',
-		    cache: false,
-		    jsonp: 'paintComments',
-		    success: function(data){
-		    	console.log(data);
-		    },
-		   	error: function (x, t, r) { 
-		   		// alert(x.response.message); 
-		   	}
-		});
+$('li a[href*="more"]').bind('click', function(event){
+	console.log('trigger');
+
+   $.ajax({
+	    url: "http://ec2-54-149-119-45.us-west-2.compute.amazonaws.com:3000?callback=paintComments",
+	    type: 'GET',
+	    dataType: 'jsonp',
+	    cache: false,
+	    jsonp: 'paintComments',
+	    success: function(data){
+	    	console.log(data);
+	    },
+	   	error: function (x, t, r) { 
+	   		// alert(x.response.message); 
+	   	}
 	});
-
 });
 
 function paintComments(commentObj){
